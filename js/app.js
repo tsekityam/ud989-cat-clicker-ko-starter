3ef3d42070c370f620c1b32788ed10ccfb907d71
Tse Kit Yam <me@kytse.com>
2016-10-19 23:01:01 +0800

    Move cat model to a new model object

--- js/app.js
+++ js/app.js
@@ -1,33 +1,35 @@
-var ViewModel = function() {
+var Model = function() {
   this.clickCount = ko.observable(0);
   this.name = ko.observable('Tabby');
   this.imgSrc = ko.observable('img/434164568_fea0ad4013_z.jpg');
 
+    this.level = ko.computed(function() {
+      if (this.clickCount() < 1) {
+        return "A";
+      } else if (this.clickCount() < 2) {
+        return "B";
+      } else if (this.clickCount() < 4) {
+        return "C";
+      } else if (this.clickCount() < 8) {
+        return "D";
+      } else if (this.clickCount() < 16) {
+        return "E";
+      } else if (this.clickCount() < 32) {
+        return "F";
+      }
+    }, this);
+
+    this.nicknames = ko.observableArray([
+      {nickname: 'Apple'},
+      {nickname: 'Banana'},
+      {nickname: 'Orange'}
+    ]);
+}
+var ViewModel = function() {
+  this.currentCat = ko.observable(new Model());
   this.incrementCounter = function() {
-    this.clickCount(this.clickCount() + 1);
+    this.currentCat().clickCount(this.currentCat().clickCount() + 1);
   };
-
-  this.level = ko.computed(function() {
-    if (this.clickCount() < 1) {
-      return "A";
-    } else if (this.clickCount() < 2) {
-      return "B";
-    } else if (this.clickCount() < 4) {
-      return "C";
-    } else if (this.clickCount() < 8) {
-      return "D";
-    } else if (this.clickCount() < 16) {
-      return "E";
-    } else if (this.clickCount() < 32) {
-      return "F";
-    }
-  }, this);
-
-  this.nicknames = ko.observableArray([
-    {nickname: 'Apple'},
-    {nickname: 'Banana'},
-    {nickname: 'Orange'}
-  ]);
 }
 
 ko.applyBindings(new ViewModel());
