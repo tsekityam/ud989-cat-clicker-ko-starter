a446f78e4754e076fbfa62a1831efec0bec58789
Tse Kit Yam <me@kytse.com>
2016-10-19 21:48:03 +0800

    Implement cat level
    This is used to practice using computed observable of Knokout

--- js/app.js
+++ js/app.js
@@ -6,6 +6,22 @@ var ViewModel = function() {
   this.incrementCounter = function() {
     this.clickCount(this.clickCount() + 1);
   };
+
+  this.level = ko.computed(function() {
+    if (this.clickCount() < 1) {
+      return "A";
+    } else if (this.clickCount() < 2) {
+      return "B";
+    } else if (this.clickCount() < 4) {
+      return "C";
+    } else if (this.clickCount() < 8) {
+      return "D";
+    } else if (this.clickCount() < 16) {
+      return "E";
+    } else if (this.clickCount() < 32) {
+      return "F";
+    }
+  }, this);
 }
 
 ko.applyBindings(new ViewModel());
